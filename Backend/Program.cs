5876686d8e17ef96f96dc5a1a39912242aed4217
Anna <magkoevaanna@gmail.com>
2026-07-01 20:30:14 +0300

    Настроена строка подключения к Clever Cloud и запуск репозитория в Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,10 +1,27 @@
+using UniversityLibrary.Backend.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+string? connectionString = Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    connectionString = builder.Configuration.GetConnectionString("MySqlConnection")!;
+}
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); 
+
+
+builder.Services.AddSingleton<IBookRepository>(new BookRepository(connectionString));
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
+
 
 
 if (app.Environment.IsDevelopment())
@@ -14,8 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
+app.MapControllers();
+app.UseCors();
 app.Run();
 
 
